7eef859a0fd5aac9a6a5a8c2335996c342ec2d2e
Azzapop <aaron@github.abhook.id.au>
2024-07-24 22:11:08 +1000

    linting

--- src/libs/async-handler.ts
+++ src/libs/async-handler.ts
@@ -1,7 +1,9 @@
-import { RequestHandler } from 'express';
+import type { RequestHandler } from 'express';
 
 // TODO better typing here
+/* eslint-disable @typescript-eslint/no-explicit-any */
 type Handler = RequestHandler<any, any, any, any, any>;
+/* eslint-enable @typescript-eslint/no-explicit-any */
 
 export const asyncHanlder = (handler: Handler) => {
   const wrapper: Handler = (req, res, next): Promise<void> => {
