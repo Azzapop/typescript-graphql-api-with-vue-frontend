227e67bd75e7a1f9d1c4a6662dbcf4d9b25fc942
Azzapop <aaron@github.abhook.id.au>
2024-09-24 21:17:37 +1000

    Use graphql schema properly

--- src/graphql/createSchema.ts
+++ src/graphql/createSchema.ts
@@ -1,7 +1,7 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import type { GraphQLSchema } from 'graphql';
 import { resolvers } from './resolvers';
-import { typeDefs } from './type-defs';
+import typeDefs from './type-defs.graphql';
 
 export const createSchema = (): GraphQLSchema => {
   return makeExecutableSchema({
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import graphqlLoader from 'vite-plugin-graphql-loader'
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), vue()],
+  plugins: [tsconfigPaths(), vue(), graphqlLoader()],
   optimizeDeps: { exclude: ['fsevents'] },
   build: {
     target: 'esnext',
