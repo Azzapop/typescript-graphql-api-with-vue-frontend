4478a027261f6cf5cba6ce507e6a70e93485da4b
Azzapop <aaron@github.abhook.id.au>
2024-10-25 23:51:36 +1100

    Updated styles up to menu sidebar

--- src/modules/client/app/layout/AppFooter.vue
+++ src/modules/client/app/layout/AppFooter.vue
@@ -1,8 +1,25 @@
 <script setup></script>
 
 <template>
-    <div class="layout-footer">
-        SAKAI by
-        <a href="https://primevue.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeVue</a>
-    </div>
+  <div class="layout-footer">
+    SAKAI by
+    <a
+      href="https://primevue.org"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="text-primary font-bold hover:underline"
+      >PrimeVue</a
+    >
+  </div>
 </template>
+
+<style lang="scss">
+.layout-footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem 0 1rem 0;
+  gap: 0.5rem;
+  border-top: 1px solid var(--surface-border);
+}
+</style>
--- src/modules/client/app/layout/AppMenuItem.vue
+++ src/modules/client/app/layout/AppMenuItem.vue
@@ -20,6 +20,10 @@ const props = defineProps({
     type: Boolean,
     default: true,
   },
+  depth: {
+    type: Number,
+    default: 0,
+  },
   parentItemKey: {
     type: String,
     default: null,
@@ -82,49 +86,55 @@ function checkActiveRoute(item) {
 </script>
 
 <template>
-  <li
-    :class="{ 'layout-root-menuitem': root, 'active-menuitem': isActiveMenu }"
-  >
-    <div
-      v-if="root && item.visible !== false"
-      class="layout-menuitem-root-text"
-    >
+  <li>
+    <!-- If we're in the root, just show the label as is -->
+    <div v-if="root && item.visible !== false" class="layout-menuitem__label">
       {{ item.label }}
     </div>
+    <!-- If not in the root, and this has sublevels, render a "button" -->
     <a
-      v-if="(!item.to || item.items) && item.visible !== false"
+      v-if="!root && (!item.to || item.items) && item.visible !== false"
       :href="item.url"
       @click="itemClick($event, item, index)"
-      :class="item.class"
+      :class="[item.class, 'layout-menuitem__anchor', `layout-menuitem__anchor--depth-${depth}`]"
       :target="item.target"
       tabindex="0"
     >
-      <i :class="item.icon" class="layout-menuitem-icon"></i>
-      <span class="layout-menuitem-text">{{ item.label }}</span>
+      <i :class="item.icon" class="layout-menuitem__icon"></i>
+      <span>{{ item.label }}</span>
       <i
-        class="pi pi-fw pi-angle-down layout-submenu-toggler"
+        class="pi pi-fw pi-angle-down layout-menuitem__submenu-toggler"
+        :class="{ 'layout-menuitem__submenu-toggler--active': isActiveMenu }"
         v-if="item.items"
       ></i>
     </a>
+    <!-- If no submenu, render a router link directly -->
     <router-link
       v-if="item.to && !item.items && item.visible !== false"
       @click="itemClick($event, item, index)"
-      :class="[item.class, { 'active-route': checkActiveRoute(item) }]"
+      :class="[
+        item.class,
+        'layout-menuitem__anchor',
+        `layout-menuitem__anchor--depth-${depth}`,
+        { 'layout-menuitem__anchor--active': checkActiveRoute(item) },
+      ]"
       tabindex="0"
       :to="item.to"
     >
-      <i :class="item.icon" class="layout-menuitem-icon"></i>
-      <span class="layout-menuitem-text">{{ item.label }}</span>
+      <i :class="item.icon" class="layout-menuitem__icon"></i>
+      <span>{{ item.label }}</span>
       <i
-        class="pi pi-fw pi-angle-down layout-submenu-toggler"
+        class="pi pi-fw pi-angle-down layout-menuitem__submenu-toggler"
+        :class="{ 'layout-menuitem__submenu-toggler--active': isActiveMenu }"
         v-if="item.items"
       ></i>
     </router-link>
+    <!-- Recursively render sub menus -->
     <Transition
       v-if="item.items && item.visible !== false"
-      name="layout-submenu"
+      name="layout-menuitem__submenu"
     >
-      <ul v-show="root ? true : isActiveMenu" class="layout-submenu">
+    <ul v-show="root ? true : isActiveMenu" :class="['layout-menuitem__submenu', `layout-menuitem__submenu--depth-${depth}`]">
         <app-menu-item
           v-for="(child, i) in item.items"
           :key="child"
@@ -132,10 +142,125 @@ function checkActiveRoute(item) {
           :item="child"
           :parentItemKey="itemKey"
           :root="false"
+          :depth="depth + 1"
         ></app-menu-item>
       </ul>
     </Transition>
   </li>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+@mixin focused-inset() {
+  outline-offset: -1px;
+  box-shadow: inset var(--focus-ring-shadow);
+}
+
+.layout-menuitem {
+  &__anchor {
+    user-select: none;
+    display: flex;
+    align-items: center;
+    position: relative;
+    outline: 0 none;
+    color: var(--text-color);
+    cursor: pointer;
+    padding: 0.75rem 1rem;
+    border-radius: var(--content-border-radius);
+    transition:
+      background-color var(--element-transition-duration),
+      box-shadow var(--element-transition-duration);
+
+    &--active {
+      font-weight: 700;
+      color: var(--primary-color);
+    }
+
+    &--depth-2 {
+      margin-left: 1rem;
+    }
+
+    &--depth-3 {
+      margin-left: 2rem;
+    }
+
+    &--depth-4 {
+      margin-left: 2.5rem;
+    }
+
+    &--depth-5 {
+      margin-left: 3rem;
+    }
+
+    &--depth-6 {
+      margin-left: 3.5rem;
+    }
+
+    &--depth-7 {
+      margin-left: 4rem;
+    }
+
+    &:hover {
+      background-color: var(--surface-hover);
+    }
+
+    &:focus {
+      @include focused-inset();
+    }
+  }
+
+  &__label {
+    font-size: 0.857rem;
+    text-transform: uppercase;
+    font-weight: 700;
+    color: var(--text-color);
+    margin: 0.75rem 0;
+  }
+
+  &__submenu-toggler {
+    font-size: 75% !important; // To get around icon library styling
+    margin-left: auto;
+    transition: transform var(--element-transition-duration);
+
+    &--active {
+      transform: rotate(-180deg);
+    }
+  }
+
+  &__submenu {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+
+    &--depth-1 {
+      overflow: hidden;
+      border-radius: var(--content-border-radius);
+    }
+  }
+
+  &__icon {
+    margin-right: 0.5rem;
+  }
+}
+
+// The following styles are used for the transition for the submenu
+// Because of this, they do not follow BEM principles
+.layout-menuitem__submenu-enter-from,
+.layout-menuitem__submenu-leave-to {
+  max-height: 0;
+}
+
+.layout-menuitem__submenu-enter-to,
+.layout-menuitem__submenu-leave-from {
+  max-height: 1000px;
+}
+
+.layout-menuitem__submenu-leave-active {
+  overflow: hidden;
+  transition: max-height 0.45s cubic-bezier(0, 1, 0, 1);
+}
+
+.layout-menuitem__submenu-enter-active {
+  overflow: hidden;
+  transition: max-height 1s ease-in-out;
+}
+</style>
