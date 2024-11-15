4476f29a6557f6868c5ff408369d7685165b2bc0
Azzapop <aaron@github.abhook.id.au>
2024-11-15 21:21:02 +1100

    Fix linting

--- src/libs/client-utils/assert.ts
+++ src/libs/client-utils/assert.ts
@@ -1,3 +1,5 @@
+// Use function definition here as asserts doesn't work with const expressions
+// eslint-disable-next-line func-style
 export function assert<T>(t: T): asserts t is NonNullable<T> {
   if (t === undefined || t === null) {
     throw new Error('Expected value to be defined.');
