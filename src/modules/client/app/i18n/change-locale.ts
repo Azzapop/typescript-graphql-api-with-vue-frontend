d1b22f92d4d433d0bbfe9377bd5677101d301f56
Azzapop <aaron@github.abhook.id.au>
2025-07-13 19:47:45 +1000

    Fix lcoale swicher function

--- src/modules/client/app/i18n/change-locale.ts
+++ src/modules/client/app/i18n/change-locale.ts
@@ -1,22 +1,23 @@
-import { i18n } from './create-app-i18n';
 import type { Locale } from './messages';
+import type { I18n } from 'vue-i18n';
 
 const loadedLanguages = new Set<Locale>(['en']);
 
-const loadLocaleMessages = async (locale: Locale) => {
+const loadLocaleMessages = async (i18n: I18n, locale: Locale) => {
   if (loadedLanguages.has(locale)) return {};
 
   const messages = await import(
     /* @vite-ignore */ `./locales/${locale}.json`
+    // TODO actually correct m.default
   ).then(m => m.default);
 
   i18n.global.setLocaleMessage(locale, messages);
   loadedLanguages.add(locale);
 };
 
-export const changeLocale = async (newLocale: Locale) => {
+export const changeLocale = async (i18n: I18n, newLocale: Locale) => {
   if (i18n.global.locale === newLocale) return;
 
-  await loadLocaleMessages(newLocale);
+  await loadLocaleMessages(i18n, newLocale);
   i18n.global.locale = newLocale;
 };
