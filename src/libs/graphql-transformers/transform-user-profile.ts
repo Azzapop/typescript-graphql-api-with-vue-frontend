8ecf995b59ab720c650d1e49144d434674e9a688
Azzapop <aaron@github.abhook.id.au>
2026-02-21 20:48:53 +1100

    Clean up

--- src/libs/graphql-transformers/transform-user-profile.ts
+++ src/libs/graphql-transformers/transform-user-profile.ts
@@ -2,12 +2,8 @@ import type { UserProfile } from '@prisma/client';
 import type { GqlResolversTypes } from '~libs/graphql-types';
 
 export const transformUserProfile = (
-  profile: UserProfile | null
+  profile: UserProfile
 ): GqlResolversTypes['UserProfile'] | null => {
-  if (!profile) {
-    return null;
-  }
-
   return {
     id: profile.id,
     email: profile.email ?? null,
